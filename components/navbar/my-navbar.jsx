e92ce37a8a909e7d3904451f7f36b2a9c2830a0b
gonza <matgonzalez@hotmail.fr>
2023-01-03 22:54:50 +0100

    fix: bootstrap nav link

--- components/navbar/my-navbar.jsx
+++ components/navbar/my-navbar.jsx
@@ -4,8 +4,10 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function MyNavbar() {
+    const router = useRouter()
     return (
         <Container>
             <Row>
@@ -14,19 +16,19 @@ export default function MyNavbar() {
                         <Navbar.Brand>FooTixiFy</Navbar.Brand>
                         SOCCER PREDICTIONS
                         <Nav>
-                            <Nav.Item>
-                                <Link href="/">Home</Link>
-                            </Nav.Item>
+                            <Nav.Link onClick={() => router.push('/')}>
+                                Home
+                            </Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Item>
-                                <Link href="/predictions">Today Predictions</Link>
-                            </Nav.Item>
+                            <Nav.Link onClick={() => router.push('/predictions')}>
+                                Upcomming matches Predictions
+                            </Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Item>
-                                <Link href="/results">Historics Results</Link>
-                            </Nav.Item>
+                            <Nav.Link onClick={() => router.push('/results')}>
+                                Prediction History
+                            </Nav.Link>
                         </Nav>
                     </Container>
                 </Navbar>
