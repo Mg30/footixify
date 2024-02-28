b796789297b320712af6a027cc70fbc8c7ebee5c
gonza <matgonzalez@hotmail.fr>
2024-02-28 21:11:23 +0100

    feat: support button

--- components/navbar/my-navbar.jsx
+++ components/navbar/my-navbar.jsx
@@ -6,8 +6,25 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Button, Box } from '@mui/material';
+import LocalCafeIcon from '@material-ui/icons/LocalCafe';
+
 export default function MyNavbar() {
-    const router = useRouter()
+    // Custom style for the Buy Me a Coffee button and text
+    const supportStyle = {
+        display: 'flex', // Aligns the icon and text horizontally
+        alignItems: 'center', // Centers them vertically
+        color: '#FFD700', // Gold color for the icon and text
+        backgroundColor: '#ffffff', // White background to make it pop
+        padding: '5px 10px', // Adds some padding around the content
+        borderRadius: '20px', // Rounded corners for the background
+        textDecoration: 'none', // Removes underline from the text
+        '&:hover': {
+            backgroundColor: '#FFD700', // Gold background on hover
+            color: '#000000', // Change icon and text color on hover for contrast
+        },
+        margin: '0 10px', // Adds some space around the button and text
+    };
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -24,10 +41,16 @@ export default function MyNavbar() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         FooTixiFy
                     </Typography>
-                    <Button color="inherit" onClick={() => router.push('/predictions')} >Upcoming matches predictions</Button>
+
+                    <Box component="a" sx={supportStyle} href="https://www.buymeacoffee.com/matgonzalep" target="_blank">
+                        <LocalCafeIcon sx={{ marginRight: '5px' }} /> {/* Adds a little space between the icon and the text */}
+                        Support FooTixiFy
+                    </Box>
+
+                    <Button color="inherit" onClick={() => router.push('/predictions')}>Upcoming matches predictions</Button>
                     <Button color="inherit" onClick={() => router.push('/results')}>Predictions History</Button>
                 </Toolbar>
             </AppBar>
         </Box>
-    )
+    );
 }
