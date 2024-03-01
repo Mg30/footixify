34c64a6c4c6a426b40d571532d3d6c93aefb89db
gonza <matgonzalez@hotmail.fr>
2024-03-01 19:13:39 +0100

    fix: dependencies

--- database/duckdb.js
+++ database/duckdb.js
@@ -1,4 +1,4 @@
-import duckdb from 'duckdb';
+import duckdb from 'duckdb-lambda-x86';
 
 
 const duckdbFactory = async () => {
@@ -21,8 +21,8 @@ const duckdbFactory = async () => {
     await execute("SET s3_region='eu-west-3';")
     await execute(`SET s3_access_key_id='${process.env.AWS_ACCESS_KEY_ID}';`)
     await execute(`SET s3_secret_access_key='${process.env.AWS_SECRET_ACCESS_KEY}';`)
-    
-    return {execute, close}
+
+    return { execute, close }
 }
 
 
